342eeb013363ba2d1a26fdf9473096cac456e7a7
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-09-30 02:21:36 +0530

    update changes

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://cc-21s8.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
